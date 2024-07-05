fa4c1ba7573e3dcf4c04c3567e0259ae96e4f032
bang9878 <39482774+bang9878@users.noreply.github.com>
2024-07-05 18:02:12 +0900

    [Bronze V] Title: A-B, Time: 204 ms, Memory: 17680 KB -BaekjoonHub

---
+++ 백준/Bronze/1001. A－B/A－B.java
@@ -0,0 +1,14 @@
+
+import java.util.*;
+import java.io.*;
+import java.math.*;
+
+class Main {
+	public static void main(String args[]) throws Exception {
+		Scanner sc = new Scanner(System.in);
+		
+		int ko = sc.nextInt();
+		int bang = sc.nextInt();
+		System.out.print(ko - bang);
+	}
+}
