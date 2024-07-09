d5cad0beb677620b8b6d4ac6927da2f42cc268fd
bang9878 <39482774+bang9878@users.noreply.github.com>
2024-07-09 20:36:28 +0900

    [Silver III] Title: N과 M (4), Time: 780 ms, Memory: 23480 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.java
@@ -0,0 +1,44 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.io.FileInputStream;
+import java.util.StringTokenizer;
+
+
+class Main {
+    static int N, M;
+    static int[] check;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        check = new int[N + 1];
+        combination(1, M);
+    }
+
+    static void combination(int start, int r) {
+        if (r == 0) {
+            for (int i = 1; i <= N; i++) {
+                for(int j = 0; j < check[i]; j++){
+                    System.out.print(i + " ");
+                }
+            }
+            System.out.println();
+            return;
+        }
+
+        for (int i = start; i <= N; i++) {
+            check[i]++;
+            combination(i, r-1);
+            check[i]--;
+        }
+    }
+}
+
