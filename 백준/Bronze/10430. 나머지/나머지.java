2be005e6ac5702cbc91a069eae16df9a85a9b188
bang9878 <39482774+bang9878@users.noreply.github.com>
2024-08-11 21:10:55 +0900

    [Bronze V] Title: 나머지, Time: 124 ms, Memory: 14088 KB -BaekjoonHub

---
+++ 백준/Bronze/10430. 나머지/나머지.java
@@ -0,0 +1,26 @@
+import org.w3c.dom.ls.LSOutput;
+
+import java.io.*;
+import java.util.*;
+import java.math.*;
+
+public class Main {
+    static int n;
+    static int[][] triangle;
+    static int max = 0;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        int c = Integer.parseInt(st.nextToken());
+
+        System.out.println((a+b)%c);
+        System.out.println(((a%c) + (b%c))%c);
+        System.out.println((a*b)%c);
+        System.out.println(((a%c) * b%c)%c);
+    }
+}
+
