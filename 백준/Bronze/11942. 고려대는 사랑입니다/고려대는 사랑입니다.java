8b088dc8bcfc10d3b2ee7f94b32ffb34e5dea211
bang9878 <39482774+bang9878@users.noreply.github.com>
2024-08-14 20:14:25 +0900

    [Bronze V] Title: 고려대는 사랑입니다, Time: 84 ms, Memory: 13844 KB -BaekjoonHub

---
+++ 백준/Bronze/11942. 고려대는 사랑입니다/고려대는 사랑입니다.java
@@ -0,0 +1,11 @@
+import org.w3c.dom.ls.LSOutput;
+
+import java.io.*;
+import java.util.*;
+import java.math.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        System.out.println("고려대학교");
+    }
+}
