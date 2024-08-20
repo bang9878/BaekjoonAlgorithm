6ea3f4c683f3e63c00da7ef7c500aa92f8158148
bang9878 <39482774+bang9878@users.noreply.github.com>
2024-08-20 19:12:52 +0900

    [Silver I] Title: IOIOI, Time: 252 ms, Memory: 20076 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Silver/5525. IOIOI/IOIOI.java
@@ -0,0 +1,38 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws NumberFormatException, IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        int S = Integer.parseInt(br.readLine());
+        String M = br.readLine();
+
+        int cnt = 0;
+        int ans = 0;
+
+        for (int i = 1; i < S - 1;) {
+            if(M.charAt(i) == 'O' && M.charAt(i+1) == 'I'){
+                cnt++;
+
+                if(cnt == N){
+                    if(M.charAt(i - (cnt*2-1)) == 'I'){
+                        ans++;
+                    }
+                    cnt--;
+                }
+                i+=2;
+            }
+
+            else{
+                cnt = 0;
+                i++;
+            }
+        }
+
+        System.out.println(ans);
+
+    }
+}
