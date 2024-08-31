47733ef525864f13b2496a07de688a665f428e1b
bang9878 <39482774+bang9878@users.noreply.github.com>
2024-08-31 13:19:28 +0900

    [Bronze V] Title: 마이크로소프트 로고, Time: 96 ms, Memory: 13972 KB -BaekjoonHub

---
+++ 백준/Bronze/5338. 마이크로소프트 로고/마이크로소프트 로고.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.lang.reflect.Array;
+import java.math.BigInteger;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws NumberFormatException, IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.println("       _.-;;-._\n" + //
+                        "'-..-'|   ||   |\n" + //
+                        "'-..-'|_.-;;-._|\n" + //
+                        "'-..-'|   ||   |\n" + //
+                        "'-..-'|_.-''-._|");
+
+    }
+}
