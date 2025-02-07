c48e95ba18871c86b0adb0fe0398f95c601091a4
bang9878 <39482774+bang9878@users.noreply.github.com>
2025-02-07 21:03:45 +0900

    [Bronze II] Title: 사각형 면적, Time: 160 ms, Memory: 18444 KB, Score: 40 point -BaekjoonHub

---
+++ 백준/Bronze/22341. 사각형 면적/사각형 면적.java
@@ -0,0 +1,42 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws FileNotFoundException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		int N = Integer.parseInt(st.nextToken());
+		int C = Integer.parseInt(st.nextToken());
+
+		int curWidth = N;
+		int curHeight = N;
+		for (int i = 0; i < C; i++) {
+			st = new StringTokenizer(br.readLine());
+
+			int x = Integer.parseInt(st.nextToken());
+			int y = Integer.parseInt(st.nextToken());
+
+			if (x >= curWidth || y >= curHeight) {
+				continue;
+			}
+
+			if (curWidth * y >= curHeight * x) {
+				curHeight = y;
+			} else {
+				curWidth = x;
+			}
+		}
+
+		System.out.println(curWidth * curHeight);
+
+		// 가로 선택 가로: N 세로 y
+		// 세로 선택 가로: x 세로 N
+
+	}
+
+}
