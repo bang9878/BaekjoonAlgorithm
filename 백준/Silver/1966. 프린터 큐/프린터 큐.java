90356b09646b74919555ff2ae6da6824d348a251
bang9878 <39482774+bang9878@users.noreply.github.com>
2025-02-17 08:57:39 +0900

    [Silver III] Title: 프린터 큐, Time: 120 ms, Memory: 14780 KB -BaekjoonHub

--- 백준/Silver/1966. 프린터 큐/프린터 큐.java
+++ 백준/Silver/1966. 프린터 큐/프린터 큐.java
@@ -1,75 +1,66 @@
-
-import java.util.*;
 import java.io.*;
-import java.math.*;
+import java.util.*;
 
-class Main {
-	public static void main(String args[]) throws Exception {
+public class Main {
+	
+	static class Data {
+		int index;
+		int priority;
 		
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
-		
-		int T = Integer.parseInt(br.readLine());
-		
-		for(int test_case = 0; test_case < T; test_case++) {
-			st = new StringTokenizer(br.readLine());
-			
-			int N = Integer.parseInt(st.nextToken());
-			int idx = Integer.parseInt(st.nextToken());
-			 
-			int cnt = 0;
-			
-			ArrayList<node> arr = new ArrayList<>();
-			
-			st = new StringTokenizer(br.readLine());
-			for(int i = 0; i < N; i++) {
-				arr.add(new node(i, Integer.parseInt(st.nextToken())));
-			}
-		
-			if(N == 1) {
-				System.out.println("1");
-				continue;
-			}
-			
-			while(true) {
-				boolean print = true;
-				for(int i=1; i<arr.size(); i++) {
-					if(arr.get(0).mvp < arr.get(i).mvp) {
-						int tmpIdx = arr.get(0).idx;
-						int tmpMvp = arr.get(0).mvp;
-						
-						arr.remove(0);
-						arr.add(new node(tmpIdx, tmpMvp));
-						
-						print = false;
-						break;
-					}
-				}
-				
-				if(print) {
-					if(arr.get(0).idx == idx) {
-						System.out.println(cnt+1);
-						break;
-					}
-					else {
-						arr.remove(0);
-						cnt++;
-					}
-				}
-			}
-			arr.clear();
-			
+		public Data(int index, int priority) {
+			this.index = index;
+			this.priority = priority;
 		}
 	}
-}
-
-
-class node{
-	int idx;
-	int mvp;
 	
-	public node(int idx, int mvp) {
-		this.idx = idx;
-		this.mvp = mvp;
+	
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+
+		
+		int T = Integer.parseInt(br.readLine());
+		for(int t = 0; t < T; t++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int N = Integer.parseInt(st.nextToken());
+			int M = Integer.parseInt(st.nextToken());
+			st = new StringTokenizer(br.readLine());
+			Queue<Data> queue = new LinkedList<>();
+			PriorityQueue<Data> priorityQueue = new PriorityQueue<>(new Comparator<Data>() {
+				public int compare(Data d1, Data d2) {
+					return (d1.priority - d2.priority)*(-1);
+				}
+			});
+			
+			for(int i = 0; i < N; i++) {
+				int priority = Integer.parseInt(st.nextToken());
+				Data d = new Data(i,priority);
+				queue.offer(d);
+				priorityQueue.offer(d);
+			}
+			
+			int result = 1; // 답 
+			while(!queue.isEmpty()) {
+				if(queue.peek().priority == priorityQueue.peek().priority) {
+					if(queue.peek().index == M) break;
+					else {
+						result++;
+						queue.poll();
+						priorityQueue.poll();
+					}
+				}
+				else queue.offer(queue.poll());
+				
+			}
+			queue.clear();
+			priorityQueue.clear();
+			
+			System.out.println(result);
+		}
+		
+		
+		
+		
 	}
 }
