991b99800588dc54b1b414effcd799e6667c2587
bang9878 <39482774+bang9878@users.noreply.github.com>
2025-03-12 21:28:16 +0900

    [Silver II] Title: 풍선 공장, Time: 616 ms, Memory: 96180 KB -BaekjoonHub

---
+++ 백준/Silver/15810. 풍선 공장/풍선 공장.java
@@ -0,0 +1,41 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+
+		long[] arr = new long[N];
+		long min = Integer.MAX_VALUE;
+		st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < N; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+			min = Math.min(min, arr[i]);
+		}
+
+		long left = 0;
+		long right = min * M;
+
+		while (left + 1 < right) {
+			long center = (left + right) / 2;
+			long balloon = 0;
+
+			for (int n = 0; n < N; n++) {
+				balloon += (center / arr[n]);
+			}
+
+			if (balloon >= M) {
+				right = center;
+			} else {
+				left = center;
+			}
+		}
+
+		System.out.println(right);
+
+	}
+}
