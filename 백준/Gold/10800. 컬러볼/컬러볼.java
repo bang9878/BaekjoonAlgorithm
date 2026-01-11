c52b4f473084793675ec8ef6619f93779223be7d
방준엽 <39482774+bang9878@users.noreply.github.com>
2026-01-11 15:16:33 +0900

    [Gold II] Title: 컬러볼, Time: 912 ms, Memory: 78484 KB -BaekjoonHub

--- 백준/Gold/10800. 컬러볼/컬러볼.java
+++ 백준/Gold/10800. 컬러볼/컬러볼.java
@@ -2,7 +2,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.PriorityQueue;
+import java.util.Collections;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -10,31 +10,36 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
 
-        PriorityQueue<Ball> pq = new PriorityQueue<>();
-
         int N = Integer.parseInt(br.readLine());
         int[] colorSum = new int[N + 1];
         int[] ans = new int[N + 1];
+
+        // PQ 대신 리스트에 담기
+        ArrayList<Ball> list = new ArrayList<>(N);
         for (int i = 1; i <= N; i++) {
             st = new StringTokenizer(br.readLine());
-
             int color = Integer.parseInt(st.nextToken());
             int size = Integer.parseInt(st.nextToken());
-
-            pq.add(new Ball(i, color, size));
+            list.add(new Ball(i, color, size));
         }
+
+        Collections.sort(list);
 
         int total = 0;
         ArrayList<Ball> sameList = new ArrayList<>();
-        Ball pre = pq.poll();
+
+        Ball pre = list.get(0);
         sameList.add(pre);
-        while (!pq.isEmpty()) {
-            Ball cur = pq.poll();
+        ans[pre.idx] = total - colorSum[pre.color];
+
+        for (int i = 1; i < N; i++) {
+            Ball cur = list.get(i);
+
             if (cur.size == pre.size) { // 같은 size면 반영하면 안됨
                 sameList.add(cur);
                 ans[cur.idx] = total - colorSum[cur.color];
             } else {
-                // 다른 size면 이때 반영
+                // 다른 size면 이때 이전 그룹 반영
                 for (Ball b : sameList) {
                     total += b.size;
                     colorSum[b.color] += b.size;
@@ -46,9 +51,11 @@ public class Main {
             }
         }
 
+        StringBuilder sb = new StringBuilder();
         for (int i = 1; i <= N; i++) {
-            System.out.println(ans[i]);
+            sb.append(ans[i]).append('\n');
         }
+        System.out.print(sb);
     }
 }
 
@@ -68,4 +75,3 @@ class Ball implements Comparable<Ball> {
         return Integer.compare(this.size, o.size);
     }
 }
-
