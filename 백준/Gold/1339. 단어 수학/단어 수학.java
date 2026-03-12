cdc9d0ccbde6e85c1367eaba77671b5f0818605e
방준엽 <39482774+bang9878@users.noreply.github.com>
2026-03-12 17:12:00 +0900

    [Gold IV] Title: 단어 수학, Time: 108 ms, Memory: 14236 KB -BaekjoonHub

--- 백준/Gold/1339. 단어 수학/단어 수학.java
+++ 백준/Gold/1339. 단어 수학/단어 수학.java
@@ -2,33 +2,16 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.Arrays;
 import java.util.List;
-import java.util.Map;
-import java.util.PriorityQueue;
 
 
 public class Main {
 
     static int N;
     static List<String> list = new ArrayList<>();
-    static Map<Character, Integer> map = new HashMap<>();
     static int[] alpha = new int[26];
 
-    static class Info implements Comparable<Info> {
-        char alpha;
-        int value;
-
-        public Info(char alpha, int value) {
-            this.alpha = alpha;
-            this.value = value;
-        }
-
-        @Override
-        public int compareTo(Info o) {
-            return Integer.compare(o.value, this.value);
-        }
-    }
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -38,27 +21,11 @@ public class Main {
             list.add(br.readLine());
         }
 
-        priorityCalc();
         solve();
     }
 
-    static void solve() {
-        int sum = 0;
-        for (String str : list) {
-            int digit = 1;
-            int result = 0;
-            for (int i = str.length() - 1; i >= 0; i--) {
-                char c = str.charAt(i);
-                int value = map.get(c);
-                result += digit * value;
-                digit *= 10;
-            }
-            sum += result;
-        }
-        System.out.println(sum);
-    }
 
-    static void priorityCalc() {
+    static void solve() {
         // 우선순위 계산
         for (String str : list) {
             int digit = 1;
@@ -69,17 +36,13 @@ public class Main {
             }
         }
 
-        PriorityQueue<Info> pq = new PriorityQueue<>();
-        for (int i = 0; i < 26; i++) {
-            if(alpha[i] == 0) continue;
-
-            pq.add(new Info((char) (i + 'A'), alpha[i]));
+        Arrays.sort(alpha);
+        int ans = 0;
+        int value = 9;
+        for (int i = 25; i >= 0; i--) {
+            if(alpha[i] == 0) break;
+            ans += (alpha[i] * value--);
         }
-
-        int val = 9;
-        while (!pq.isEmpty()) {
-            Info poll = pq.poll();
-            map.put(poll.alpha, val--);
-        }
+        System.out.println(ans);
     }
 }
