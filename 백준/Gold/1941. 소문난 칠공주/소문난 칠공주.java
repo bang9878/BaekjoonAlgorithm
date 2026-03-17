6c1a0623b409c437e924ce037d9c4eb8afe5cdba
방준엽 <39482774+bang9878@users.noreply.github.com>
2026-03-17 17:19:01 +0900

    [Gold III] Title: 소문난 칠공주, Time: 376 ms, Memory: 81636 KB -BaekjoonHub

--- 백준/Gold/1941. 소문난 칠공주/소문난 칠공주.java
+++ 백준/Gold/1941. 소문난 칠공주/소문난 칠공주.java
@@ -1,11 +1,8 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.LinkedList;
 import java.util.Queue;
-
 
 public class Main {
 
@@ -13,37 +10,25 @@ public class Main {
     static int[] dirR = {-1, 1, 0, 0};
     static int[] dirC = {0, 0, -1, 1};
     static int ans = 0;
-    static class Pos {
-        int r;
-        int c;
-
-        public Pos(int r, int c) {
-            this.r = r;
-            this.c = c;
-        }
-
-    }
 
     public static void main(String[] args) throws IOException {
         input();
-        combination(0, 0, 0, new boolean[5][5]);
+        combination(0, 0, 0, 0);
         System.out.println(ans);
     }
 
-    static void combination(int start, int depth, int doyeonCnt, boolean[][] selected) {
-        if (doyeonCnt > 3) {
-            return;
-        }
+    // start: 시작 인덱스
+    // depth: 선택된 개수
+    // yCnt: Y 개수
+    // mask: 선택된 상태 (비트마스크)
+    static void combination(int start, int depth, int yCnt, int mask) {
+        // 가지치기
+        if (yCnt > 3) return;
 
-
+        // 7개 선택 완료
         if (depth == 7) {
-            for (int i = 0; i < 5; i++) {
-                for (int j = 0; j < 5; j++) {
-                    if (selected[i][j]) {
-                        bfs(selected, i, j);
-                        return;
-                    }
-                }
+            if (isConnected(mask)) {
+                ans++;
             }
             return;
         }
@@ -52,50 +37,61 @@ public class Main {
             int r = i / 5;
             int c = i % 5;
 
-            if (!selected[r][c]) {
-                selected[r][c] = true;
-                if (classes[r][c] == 'Y') {
-                    combination(i + 1, depth + 1, doyeonCnt + 1, selected);
-                } else {
-                    combination(i + 1, depth + 1, doyeonCnt, selected);
-                }
-
-                selected[r][c] = false;
+            if (classes[r][c] == 'Y') {
+                combination(i + 1, depth + 1, yCnt + 1, mask | (1 << i));
+            } else {
+                combination(i + 1, depth + 1, yCnt, mask | (1 << i));
             }
         }
     }
 
-    static void bfs(boolean[][] selected, int startR, int startC) {
-        Queue<Pos> q = new LinkedList<>();
-        boolean[][] visited = new boolean[5][5];
-        q.add(new Pos(startR, startC));
-        visited[startR][startC] = true;
+    // 연결 여부 확인 (BFS)
+    static boolean isConnected(int mask) {
+        boolean[] visited = new boolean[25];
+        Queue<Integer> q = new LinkedList<>();
 
-        int cnt = 1;
-        while (!q.isEmpty()) {
-            Pos cur = q.poll();
-
-            if (cnt == 7) {
-                ans++;
-                return;
-            }
-
-            for (int d = 0; d < 4; d++) {
-                int nextR = cur.r + dirR[d];
-                int nextC = cur.c + dirC[d];
-
-                if(!isIn(nextR, nextC) || visited[nextR][nextC] || !selected[nextR][nextC]) continue;
-                cnt++;
-                visited[nextR][nextC] = true;
-                q.add(new Pos(nextR, nextC));
+        // 시작점 찾기 (첫 번째 선택된 노드)
+        for (int i = 0; i < 25; i++) {
+            if ((mask & (1 << i)) != 0) {
+                q.add(i);
+                visited[i] = true;
+                break;
             }
         }
+
+        int cnt = 1;
+
+        while (!q.isEmpty()) {
+            int cur = q.poll();
+            int r = cur / 5;
+            int c = cur % 5;
+
+            for (int d = 0; d < 4; d++) {
+                int nr = r + dirR[d];
+                int nc = c + dirC[d];
+
+                if (!isIn(nr, nc)) continue;
+
+                int next = nr * 5 + nc;
+
+                // 선택된 칸이 아니면 skip
+                if ((mask & (1 << next)) == 0) continue;
+
+                // 이미 방문했으면 skip
+                if (visited[next]) continue;
+
+                visited[next] = true;
+                q.add(next);
+                cnt++;
+            }
+        }
+
+        return cnt == 7;
     }
 
     static boolean isIn(int r, int c) {
         return 0 <= r && r < 5 && 0 <= c && c < 5;
     }
-
 
     static void input() throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -106,7 +102,5 @@ public class Main {
                 classes[i][j] = line.charAt(j);
             }
         }
-
     }
-
 }
