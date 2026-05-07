e28476c5e59ada5bdbca71d82a9e0087f40683a9
방준엽 <39482774+bang9878@users.noreply.github.com>
2026-05-07 18:03:06 +0900

    [level 3] Title: 미로 탈출 명령어, Time: 814.48 ms, Memory: 385 MB -BaekjoonHub

---
+++ 프로그래머스/3/150365. 미로 탈출 명령어/미로 탈출 명령어.java
@@ -0,0 +1,71 @@
+import java.util.ArrayDeque;
+import java.util.Queue;
+import java.util.Set;
+import java.util.TreeSet;
+
+class Solution {
+
+    public String solution(int n, int m, int x, int y, int r, int c, int k) {
+        String answer = "";
+
+        boolean[][][] visited = new boolean[n + 1][m + 1][k  + 1];
+        Set<String> candidate = new TreeSet<>();
+        Queue<Node> q = new ArrayDeque<>();
+        q.add(new Node(x, y, 0, ""));
+        visited[x][y][0] = true;
+
+        // 하좌우상
+        int[] dirR = {1, 0, 0, -1};
+        int[] dirC = {0, -1, 1, 0};
+        String[] cmdArr = {"d", "l", "r", "u"};
+        boolean isImpossible = true;
+        while (!q.isEmpty()) {
+            Node cur = q.poll();
+
+            if (cur.r == r && cur.c == c && cur.cost == k) {
+                isImpossible = false;
+                candidate.add(cur.cmd);
+            }
+
+            for (int i = 0; i < 4; i++) {
+                int nextR = cur.r + dirR[i];
+                int nextC = cur.c + dirC[i];
+
+                if(!isIn(nextR, nextC, n, m) || cur.cost + 1 > k || visited[nextR][nextC][cur.cost + 1]) continue;
+
+                q.offer(new Node(nextR, nextC, cur.cost + 1, cur.cmd + cmdArr[i]));
+                visited[nextR][nextC][cur.cost + 1] = true;
+            }
+
+        }
+
+        if (!isImpossible) {
+            answer = candidate.stream()
+                    .findFirst()
+                    .get();
+        } else {
+            answer = "impossible";
+        }
+
+        return answer;
+    }
+
+    private boolean isIn(int r, int c, int R, int C) {
+        return 0 < r && r <= R && 0 < c && c <= C;
+    }
+}
+
+class Node {
+
+    int r;
+    int c;
+    int cost;
+    String cmd;
+
+    public Node(int r, int c, int cost, String cmd) {
+        this.r = r;
+        this.c = c;
+        this.cost = cost;
+        this.cmd = cmd;
+    }
+}
