b0f5753c51e3508a79628a2a28606433e465af45
방준엽 <39482774+bang9878@users.noreply.github.com>
2026-05-14 17:19:00 +0900

    [level 3] Title: 표 병합, Time: 7.38 ms, Memory: 60 MB -BaekjoonHub

--- 프로그래머스/3/150366. 표 병합/표 병합.java
+++ 프로그래머스/3/150366. 표 병합/표 병합.java
@@ -1,210 +1,150 @@
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 class Solution {
-    static List<String> list = new ArrayList<>();
-    static String[][] table = new String[51][51];
-    static int[][] mergeStatusArr = new int[51][51];
-    static int curMergeId = 0;
+
+    private static final int SIZE = 50;
+    private static final int CELL_COUNT = SIZE * SIZE;
+    private static final String EMPTY = "";
+
+    private int[] parent;
+    private String[] value;
+    private List<String> answer;
 
     public String[] solution(String[] commands) {
         init();
-        for (String cmd : commands) {
-            String[] splitStr = cmd.split(" ");
 
-//            for (String str : splitStr) {
-//                System.out.print(str + " ");
-//            }
-//
-//            for (int i = 0; i < 51; i++) {
-//                for (int j = 0; j < 51; j++) {
-//                    System.out.print(table[i][j] + " ");
-//                }
-//                System.out.println();
-//            }
+        for (String command : commands) {
+            String[] parts = command.split(" ");
 
+            switch (parts[0]) {
+                case "UPDATE":
+                    if (parts.length == 4) {
+                        int r = Integer.parseInt(parts[1]);
+                        int c = Integer.parseInt(parts[2]);
+                        String newValue = parts[3];
 
-            // 1, "UPDATE r c value"
-            if (splitStr[0].equals("UPDATE") && splitStr.length == 4) {
-                int r = Integer.parseInt(splitStr[1]);
-                int c = Integer.parseInt(splitStr[2]);
-                String value = splitStr[3];
-                updateRC(r, c, value);
-            }
+                        updateCell(r, c, newValue);
+                    } else {
+                        String oldValue = parts[1];
+                        String newValue = parts[2];
 
-            // 2. "UPDATE value1 value2"
-            else if (splitStr[0].equals("UPDATE") && splitStr.length == 3) {
-                String value1 = splitStr[1];
-                String value2 = splitStr[2];
-                updateV1ToV2(value1, value2);
-            }
-
-            // 3. "MERGE r1 c1 r2 c2"
-            else if (splitStr[0].equals("MERGE")) {
-                int r1 = Integer.parseInt(splitStr[1]);
-                int c1 = Integer.parseInt(splitStr[2]);
-                int r2 = Integer.parseInt(splitStr[3]);
-                int c2 = Integer.parseInt(splitStr[4]);
-                String value = "";
-
-                // 같은 셀이면 무시
-                if (r1 == r2 && c1 == c2) continue;
-
-                if (!table[r1][c1].isEmpty() && !table[r2][c2].isEmpty()) {
-                    value = table[r1][c1];
-                } else if (!table[r1][c1].isEmpty() && table[r2][c2].isEmpty()) {
-                    value = table[r1][c1];
-                } else if (table[r1][c1].isEmpty() && !table[r2][c2].isEmpty()) {
-                    value = table[r2][c2];
-                }
-
-                mergeShell(r1, c1, r2, c2, value);
-            }
-
-            // 4. "UNMERGE r c"
-            else if (splitStr[0].equals("UNMERGE")) {
-                int r = Integer.parseInt(splitStr[1]);
-                int c = Integer.parseInt(splitStr[2]);
-                unmergeShell(r, c);
-            }
-
-            // 5. "PRINT r c"
-            else if (splitStr[0].equals("PRINT")) {
-                int r = Integer.parseInt(splitStr[1]);
-                int c = Integer.parseInt(splitStr[2]);
-                printShell(r, c);
-            }
-
-//            for (int i = 0; i < 51; i++) {
-//                for (int j = 0; j < 51; j++) {
-//                    System.out.print(table[i][j] + " ");
-//                }
-//                System.out.println();
-//            }
-
-        }
-
-        return list.toArray(new String[0]);
-    }
-
-    private void updateRC(int r, int c, String value) {
-        int mergeId = mergeStatusArr[r][c];
-
-        if (mergeId == 0) {
-            table[r][c] = value;
-            return;
-        }
-
-        for (int i = 0; i < 51; i++) {
-            for (int j = 0; j < 51; j++) {
-                if (mergeStatusArr[i][j] == mergeId) {
-                    table[i][j] = value;
-                }
-            }
-        }
-    }
-
-    private void updateV1ToV2(String value1, String value2) {
-        Set<Integer> set = new HashSet<>();
-        for (int i = 0; i < 51; i++) {
-            for (int j = 0; j < 51; j++) {
-                if (table[i][j].equals(value1)) {
-                    if (mergeStatusArr[i][j] == 0) {
-                        table[i][j] = value2;
-                        continue;
+                        updateValue(oldValue, newValue);
                     }
-                    set.add(mergeStatusArr[i][j]);
-                }
+                    break;
+
+                case "MERGE":
+                    int r1 = Integer.parseInt(parts[1]);
+                    int c1 = Integer.parseInt(parts[2]);
+                    int r2 = Integer.parseInt(parts[3]);
+                    int c2 = Integer.parseInt(parts[4]);
+
+                    merge(r1, c1, r2, c2);
+                    break;
+
+                case "UNMERGE":
+                    int r = Integer.parseInt(parts[1]);
+                    int c = Integer.parseInt(parts[2]);
+
+                    unmerge(r, c);
+                    break;
+
+                case "PRINT":
+                    int printR = Integer.parseInt(parts[1]);
+                    int printC = Integer.parseInt(parts[2]);
+
+                    print(printR, printC);
+                    break;
             }
         }
 
-        for (int mergeId : set) {
-            for (int i = 0; i < 51; i++) {
-                for (int j = 0; j < 51; j++) {
-                    if (mergeStatusArr[i][j] == mergeId) {
-                        table[i][j] = value2;
-                    }
-                }
-            }
-        }
+        return answer.toArray(new String[0]);
     }
-
-    private void mergeShell(int r1, int c1, int r2, int c2, String value) {
-        int mergeId1 = mergeStatusArr[r1][c1];
-        int mergeId2 = mergeStatusArr[r2][c2];
-
-        ++curMergeId;
-        if (mergeId1 == 0 && mergeId2 == 0) {
-            mergeStatusArr[r1][c1] = curMergeId;
-            mergeStatusArr[r2][c2] = curMergeId;
-            table[r1][c1] = value;
-            table[r2][c2] = value;
-        } else if (mergeId1 == 0) {
-            mergeStatusArr[r1][c1] = curMergeId;
-            table[r1][c1] = value;
-            updateMergeId(mergeId2, value);
-        } else if (mergeId2 == 0) {
-            mergeStatusArr[r2][c2] = curMergeId;
-            table[r2][c2] = value;
-            updateMergeId(mergeId1, value);
-        } else {
-            for (int i = 0; i < 51; i++) {
-                for (int j = 0; j < 51; j++) {
-                    if (mergeStatusArr[i][j] == mergeId1 || mergeStatusArr[i][j] == mergeId2) {
-                        mergeStatusArr[i][j] = curMergeId;
-                        table[i][j] = value;
-                    }
-                }
-            }
-        }
-    }
-
-    private void unmergeShell(int r, int c) {
-        int mergeId = mergeStatusArr[r][c];
-        String value = table[r][c];
-
-        if (mergeId == 0) {
-            return;
-        }
-        
-        
-        for (int i = 0; i < 51; i++) {
-            for (int j = 0; j < 51; j++) {
-                if (mergeStatusArr[i][j] == mergeId) {
-                    mergeStatusArr[i][j] = 0;
-                    table[i][j] = "";
-                }
-            }
-        }
-        table[r][c] = value;
-    }
-
-    private void printShell(int r, int c) {
-        if (table[r][c].isEmpty()) {
-            list.add("EMPTY");
-        } else {
-            list.add(table[r][c]);
-        }
-    }
-
-    private void updateMergeId(int mergeId, String value) {
-        for (int i = 0; i < 51; i++) {
-            for (int j = 0; j < 51; j++) {
-                if (mergeStatusArr[i][j] == mergeId) {
-                    mergeStatusArr[i][j] = curMergeId;
-                    table[i][j] = value;
-                }
-            }
-        }
-    }
-
 
     private void init() {
-        for (int i = 0; i <= 50; i++) {
-            Arrays.fill(table[i], "");
+        parent = new int[CELL_COUNT];
+        value = new String[CELL_COUNT];
+        answer = new ArrayList<>();
+
+        for (int i = 0; i < CELL_COUNT; i++) {
+            parent[i] = i;
+            value[i] = EMPTY;
         }
+    }
+
+    private void updateCell(int r, int c, String newValue) {
+        int root = find(toIndex(r, c));
+        value[root] = newValue;
+    }
+
+    private void updateValue(String oldValue, String newValue) {
+        for (int i = 0; i < CELL_COUNT; i++) {
+            if (parent[i] == i && value[i].equals(oldValue)) {
+                value[i] = newValue;
+            }
+        }
+    }
+
+    private void merge(int r1, int c1, int r2, int c2) {
+        int cell1 = toIndex(r1, c1);
+        int cell2 = toIndex(r2, c2);
+
+        int root1 = find(cell1);
+        int root2 = find(cell2);
+
+        if (root1 == root2) {
+            return;
+        }
+
+        String mergedValue = !value[root1].isEmpty()
+                ? value[root1]
+                : value[root2];
+
+        parent[root2] = root1;
+        value[root1] = mergedValue;
+        value[root2] = EMPTY;
+    }
+
+    private void unmerge(int r, int c) {
+        int target = toIndex(r, c);
+        int root = find(target);
+        String savedValue = value[root];
+
+        List<Integer> mergedCells = new ArrayList<>();
+
+        for (int i = 0; i < CELL_COUNT; i++) {
+            if (find(i) == root) {
+                mergedCells.add(i);
+            }
+        }
+
+        for (int cell : mergedCells) {
+            parent[cell] = cell;
+            value[cell] = EMPTY;
+        }
+
+        value[target] = savedValue;
+    }
+
+    private void print(int r, int c) {
+        int root = find(toIndex(r, c));
+
+        if (value[root].isEmpty()) {
+            answer.add("EMPTY");
+        } else {
+            answer.add(value[root]);
+        }
+    }
+
+    private int find(int x) {
+        if (parent[x] == x) {
+            return x;
+        }
+
+        return parent[x] = find(parent[x]);
+    }
+
+    private int toIndex(int r, int c) {
+        return (r - 1) * SIZE + (c - 1);
     }
 }
